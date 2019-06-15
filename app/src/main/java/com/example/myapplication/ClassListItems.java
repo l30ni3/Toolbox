3a00d688be44baf86a80932be210a9e822203c1a
Leonie Koch <lk.leonie.koch@gmail.com>
2019-06-15 18:11:14 +0200

    Put database connection in AsyncTask and load data into Dynamic ListView

---
+++ app/src/main/java/com/example/myapplication/ClassListItems.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+public class ClassListItems
+{
+
+    public String img; //Image URL
+    public String name; //Name
+
+    public ClassListItems(String name, String img)
+    {
+        this.img = img;
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- app/src/main/java/com/example/myapplication/ShowAllTools.java
+++ app/src/main/java/com/example/myapplication/ShowAllTools.java
@@ -2,9 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Arrays;
 
 import java.util.ArrayList;
@@ -12,39 +32,162 @@ import java.util.List;
 
 public class ShowAllTools extends AppCompatActivity {
 
-    @Override
+    private ArrayList<ClassListItems> itemArrayList;  //List items Array
+    private MyAppAdapter myAppAdapter; //Array Adapter
+    private ListView listView; // ListView
+    private boolean success = false; // boolean
+
+
+    private static final String DB_URL = "jdbc:postgresql://10.0.2.2:5432/Werkzeugausleihe";
+    private static final String USER = "postgres";
+    private static final String PASS = "postgresql";
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_all_tools);
 
+        listView = (ListView) findViewById(R.id.listAllTools); //ListView Declaration
+        itemArrayList = new ArrayList<ClassListItems>(); // Arraylist Initialization
 
-        // declaration and initialize String Array  
-        String listString = "";
+        // Calling Async Task
+        SyncData orderData = new SyncData();
+        orderData.execute("");
+    }
 
-        // Start database connection in new thread
-        DB d = new DB();
-        d.start();
+    // Async Task has three overrided methods,
+    private class SyncData extends AsyncTask<String, String, String> {
+        String msg = "Internet/DB_Credentials/Windows_FireWall_TurnOn Error, See Android Monitor in the bottom For details!";
+        ProgressDialog progress;
 
-        // Polling - to do: Future!
-        while(!d.done()) {
+        @Override
+        protected void onPreExecute() //Starts the progress dailog
+        {
+            progress = ProgressDialog.show(ShowAllTools.this, "Synchronising",
+                    "List Loading! Please Wait...", true);
+        }
+
+        @Override
+        protected String doInBackground(String... strings)  // Connect to the database, write query and add items to array list
+        {
             try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
+
+                /* Register jdbc driver class. */
+                Class.forName("org.postgresql.Driver");
+
+                /* Get the Connection object. */
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+                if (conn == null) {
+                    success = false;
+                } else {
+                    // Change below query according to your own database.
+                    String query = "SELECT * FROM test.table1";
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+                    if (rs != null) // if resultset not null, I add items to itemArraylist using class created
+                    {
+                        while (rs.next()) {
+                            try {
+                                itemArrayList.add(new ClassListItems(rs.getString("name"), rs.getString("url")));
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                        msg = "Found";
+                        success = true;
+                    } else {
+                        msg = "No Data found!";
+                        success = false;
+                    }
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
+                Writer writer = new StringWriter();
+                e.printStackTrace(new PrintWriter(writer));
+                msg = writer.toString();
+                success = false;
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) // disimissing progress dialoge, showing error and setting up my ListView
+        {
+            progress.dismiss();
+            Toast.makeText(ShowAllTools.this, msg + "", Toast.LENGTH_LONG).show();
+            if (success == false) {
+            } else {
+                try {
+                    myAppAdapter = new MyAppAdapter(itemArrayList, ShowAllTools.this);
+                    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                    listView.setAdapter(myAppAdapter);
+                } catch (Exception ex) {
+
+                }
+
             }
         }
+    }
 
-        List<String> data = d.getData();
-        String str[] = new String[data.size()];
-        for (int j = 0; j < data.size(); j++) {
-
-            // Assign each value to String array
-            str[j] = data.get(j);
+    public class MyAppAdapter extends BaseAdapter         //has a class viewholder which holds
+    {
+        public class ViewHolder {
+            //TextView textName;
+            //ImageView imageView;
+            TextView listText;
+            ImageView listImage;
         }
 
-        listString = Arrays.toString(str);
+        public List<ClassListItems> parkingList;
 
-        TextView dataView = findViewById(R.id.data);
-        dataView.setText(listString);
+        public Context context;
+        ArrayList<ClassListItems> arraylist;
+
+        private MyAppAdapter(List<ClassListItems> apps, Context context) {
+            this.parkingList = apps;
+            this.context = context;
+            arraylist = new ArrayList<ClassListItems>();
+            arraylist.addAll(parkingList);
+        }
+
+        @Override
+        public int getCount() {
+            return parkingList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) // inflating the layout and initializing widgets
+        {
+
+            View rowView = convertView;
+            ViewHolder viewHolder = null;
+            if (rowView == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                rowView = inflater.inflate(R.layout.list_content, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.listText = (TextView) rowView.findViewById(R.id.listText);
+                viewHolder.listImage = (ImageView) rowView.findViewById(R.id.listImage);
+                rowView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            // here setting up names and images
+            viewHolder.listText.setText(parkingList.get(position).getName() + "");
+            Picasso.with(context).load(parkingList.get(position).getImg()).into(viewHolder.listImage);
+
+            return rowView;
+        }
     }
 }
